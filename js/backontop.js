76f928fa85dea6587b397d169ff985f38e342764
xqian7 <xqian7@stevens.edu>
2025-04-11 16:36:32 -0400

    remove

--- js/backontop.js
+++
@@ -1,8 +0,0 @@
-
-$(function(){
-    $(".backontop").click(function(e){
-      e.preventDefault();
-      var target = $(this).attr("href");
-      $('html,body').scrollTo(target.toString(),target.toString()); 
-    });
-  });
